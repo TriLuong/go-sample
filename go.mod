b6d22a40b97e56654a26dddd0c2f34318e423d87
Tri Luong <triluongdl@gmail.com>
2019-11-24 22:57:32 +0700

    Add login.

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/TriLuong/go-sample
+
+go 1.13
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/mattn/go-colorable v0.1.4 // indirect
+	github.com/mattn/go-isatty v0.0.10 // indirect
+	github.com/valyala/fasttemplate v1.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20191122220453-ac88ee75c92c // indirect
+	golang.org/x/net v0.0.0-20191119073136-fc4aabc6c914 // indirect
+	golang.org/x/sys v0.0.0-20191120155948-bd437916bb0e // indirect
+	golang.org/x/text v0.3.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,10 +1,143 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
+
+type User struct {
+	Role      string `json:"role"`
+	Email     string `json:"email"`
+	Passoword string `json:"password"`
+	Phone     string `json:"phone"`
+	Token     string `json:"token"`
+}
+
+type JwtClaims struct {
+	id string `json:"id"`
+	jwt.StandardClaims
+}
+
+func login(c echo.Context) error {
+	var user map[string]interface{}
+
+	defer c.Request().Body.Close()
+
+	b, error := ioutil.ReadAll((c.Request().Body))
+
+	if error != nil {
+		return c.String(http.StatusInternalServerError, fmt.Sprintf("Error %s", error))
+	}
+
+	error = json.Unmarshal(b, &user)
+
+	if error != nil {
+		return c.String(http.StatusInternalServerError, fmt.Sprintf("Error %s", error))
+	}
+
+	token, error := createJwt("admin@gmail.com")
+	if error != nil {
+		return c.String(http.StatusInternalServerError, "Login ERROR!!!")
+	}
+	return c.JSON(http.StatusOK, map[string]string{
+		"token": token,
+	})
+}
+
+func createJwt(id string) (string, error) {
+	claims := JwtClaims{
+		id,
+		jwt.StandardClaims{
+			Id:        "admin_id",
+			ExpiresAt: time.Now().Add(24 * time.Hour).Unix(),
+		},
+	}
+
+	rawToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	token, error := rawToken.SignedString([]byte("go-sample"))
+	if error != nil {
+		return "", error
+	}
+	return token, nil
+}
+
+func getUsers(c echo.Context) error {
+	role := c.QueryParam("role")
+
+	return c.String(http.StatusOK, fmt.Sprintf("Get Users with role %s\n", role))
+}
+
+func getUserById(c echo.Context) error {
+	id := c.Param("id")
+
+	if id == "string" {
+		return c.String(http.StatusOK, fmt.Sprintf("Get User with ID %s\n", id))
+	}
+
+	if id == "json" {
+		return c.JSON(http.StatusOK, map[string]string{
+			"id": id,
+		})
+	}
+
+	return c.JSON(http.StatusNotFound, map[string]string{
+		"error": "Not found user",
+	})
+
+}
+
+func addUser(c echo.Context) error {
+	// user := User{}
+	var user map[string]interface{}
+
+	defer c.Request().Body.Close()
+
+	b, error := ioutil.ReadAll((c.Request().Body))
+
+	if error != nil {
+		return c.String(http.StatusInternalServerError, fmt.Sprintf("Error %s", error))
+	}
+
+	error = json.Unmarshal(b, &user)
+
+	if error != nil {
+		return c.String(http.StatusInternalServerError, fmt.Sprintf("Cannot convert %s", error))
+	}
+
+	return c.JSON(http.StatusOK, user)
+}
 
 func main() {
 	fmt.Println("Welcome Server!!!")
+	e := echo.New()
 
+	// e.Use(middleware.Logger())
+	// e.Use(middleware.BasicAuth(func(email, password string, c echo.Context) (bool, error) {
+	// 	if email == "admin@gmail.com" && password == "123456" {
+	// 		return true, nil
+	// 	}
+	// 	return false, nil
+	// }))
+	e.POST("/auth/login", login)
+
+	g := e.Group("/users")
+	g.Use(middleware.JWT([]byte("go-sample")))
+	// g.Use(middleware.JWTWithConfig(middleware.JWTConfig{
+	// 	SigningMethod: "HS256",
+	// 	SigningKey:    []byte("go-sample"),
+	// }))
+
+	g.GET("", getUsers)
+	g.POST("", addUser)
+	g.GET("/:id", getUserById)
+
+	fmt.Println("Start server")
+	e.Start(":5000")
 }
